ebbe97990fac5cb176a0270f3e56f127c6850c1c
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2022-12-01 00:07:49 +0300

    js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-
-const oldDict = require('./101-data').dict;
+const dict = require('./101-data').dict;
 const newDict = {};
-for (const k in oldDict) {
-	if (oldDict[k] in newDict) {
-		newDict[oldDict[k]].push(k);
-	} else {
-	newDict[oldDict[k]] = [k];
-	}
-}
+
+Object.keys(dict).map(function (key, index) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+});
+
 console.log(newDict);
