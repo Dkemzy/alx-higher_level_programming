4af9b3777a8ce47d14cb957485de92a1a37a28d9
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2022-11-30 23:42:33 +0300

    js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-      if (w <= 0 || h <= 0 || !w || !h) {
-        return;
-      }
-      this.width = w;
-      this.height = h;
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !w || !h) {
+      return;
     }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
-  module.exports = Rectangle;
+}
+module.exports = Rectangle;
