350ae7637fe720c9f1362cd0ed872ba76426f40f
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2022-11-30 23:53:47 +0300

    js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-
 exports.esrever = function (list) {
-    let rev = list.length - 1;
-    const newList = [];
-    for (let i = 0; i < list.length; i++) {
-      newList[rev] = list[i];
-      rev--;
-    }
-    return (newList);
-  };
+  let len = list.length - 1;
+  let i = 0;
+  while ((len - i) > 0) {
+    const aux = list[len];
+    list[len] = list[i];
+    list[i] = aux;
+    i++;
+    len--;
+  }
+  return list;
+};
