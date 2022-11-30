552fc2afd0b0ce280e70f843757fe5736bd41e77
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2022-12-01 00:08:33 +0300

    js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-	if (err) throw err;
-	fs.writeFile(process.argv[4], data, (err) => {
-		if (err) throw err;
-	});
-});
-
-fs.readFile(process.argv[3], 'utf-8', (err, data) => {
-	if (err) throw err;
-	fs.appendFile(process.argv[4], data, (err) => {
-		if (err) throw err;
-	});
-});
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
