afce187db098e27c0c7fbbd058ff46a8a3e74058
Dkemzy <Dennisworks366@gmail.com>
2023-02-12 02:16:07 +0000

    python

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,19 +3,55 @@
 Python script that shows the last 10 commits of a repository
 in GitHub
 """
-from requests import get, auth
+import requests
+
 import sys
 
 
-if __name__ == "__main__":
-    try:
-        repo = sys.argv[1]
-        owner = sys.argv[2]
-        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-        r = get(url)
-        json_o = r.json()
-        for i in range(0, 10):
-            print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
-                                  .get('author').get('name')))
-    except IndexError:
-        pass
+
+def get_commits(repo, owner):
+
+    # Base URL
+
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+
+    
+
+    # Send GET request to the URL
+
+    response = requests.get(url)
+
+    
+
+    # Check if the request was successful
+
+    if response.status_code == 200:
+
+        # Extract commits from the response
+
+        commits = response.json()
+
+        
+
+        # Print 10 most recent commits
+
+        for i in range(10):
+
+            sha = commits[i]['sha']
+
+            name = commits[i]['commit']['author']['name']
+
+            print(f"{sha}: {name}")
+
+    else:
+
+        # If the request was not successful, print the error message
+
+        print(f"Request failed with status code: {response.status_code}")
+# Read the repository name and owner name from the command line arguments
+repo = sys.argv[1]
+owner = sys.argv[2]
+# Call the get_commits function with the given repository and owner
+get_commits(repo, owner)
+
+
