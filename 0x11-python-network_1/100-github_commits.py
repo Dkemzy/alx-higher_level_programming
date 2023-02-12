6ebf18fccb750e77d5dd5f40d7097386127fd563
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2023-02-12 05:17:03 +0300

    git add

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,57 +1,30 @@
-#!/usr/bin/python3
-"""
-Python script that shows the last 10 commits of a repository
-in GitHub
-"""
 import requests
-
 import sys
 
-
-
 def get_commits(repo, owner):
-
     # Base URL
-
     url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-
     
-
     # Send GET request to the URL
-
     response = requests.get(url)
-
     
-
     # Check if the request was successful
-
     if response.status_code == 200:
-
         # Extract commits from the response
-
         commits = response.json()
-
         
-
         # Print 10 most recent commits
-
         for i in range(10):
-
             sha = commits[i]['sha']
-
             name = commits[i]['commit']['author']['name']
-
             print(f"{sha}: {name}")
-
     else:
-
         # If the request was not successful, print the error message
-
         print(f"Request failed with status code: {response.status_code}")
+
 # Read the repository name and owner name from the command line arguments
 repo = sys.argv[1]
 owner = sys.argv[2]
+
 # Call the get_commits function with the given repository and owner
 get_commits(repo, owner)
-
-
